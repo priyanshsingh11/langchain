f2bee35495cf7ec7d4cd5b8d16a1947002d4d804
Priyansh Singh <priyanshsingh855@gmail.com>
2025-12-22 14:48:22 +0530

    Initial commit

---
+++ langchain_prompts/prompt.py
@@ -0,0 +1,27 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_core.prompts import PromptTemplate,load_prompt
+
+load_dotenv()
+model = ChatOpenAI()
+
+st.header('Reasearch Tool')
+
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+# rather than doing the things maunally we just made the template and can reuse it 
+template = load_prompt('template.json')
+
+if st.button('Summarize'):
+    chain = template | model # for the cost cutting the 2 process in one place
+    result = chain.invoke({
+        'paper_input':paper_input,
+        'style_input':style_input,
+        'length_input':length_input
+    })
+    st.write(result.content)
