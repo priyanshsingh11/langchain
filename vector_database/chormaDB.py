d860d859a8a95179fc2c66d84758b1c2d99fb4f8
Priyansh Singh <priyanshsingh855@gmail.com>
2025-12-24 14:45:48 +0530

    vector database

--- vector_database/chormaDB.py
+++ vector_database/chormaDB.py
@@ -28,30 +28,39 @@ doc5 = Document(
         metadata={"team": "Chennai Super Kings"}
     )
 docs = [doc1, doc2, doc3, doc4, doc5]
+
+
 vector_store = Chroma(
-    embedding_function=OpenAIEmbeddings(),
-    persist_directory='my_chroma_db',
-    collection_name='sample'
+    embedding_function=OpenAIEmbeddings(), # embedding we used
+    persist_directory='my_chroma_db', # place we stored
+    collection_name='sample' # name of the file 
 )
+
+
 # add documents
 vector_store.add_documents(docs)
+
 # view documents
 vector_store.get(include=['embeddings','documents', 'metadatas'])
+
 # search documents
 vector_store.similarity_search(
     query='Who among these are a bowler?',
-    k=2
+    k=2 # how many of similar documents we need
 )
+
 # search with similarity score
 vector_store.similarity_search_with_score(
     query='Who among these are a bowler?',
     k=2
 )
+
 # meta-data filtering
 vector_store.similarity_search_with_score(
     query="",
     filter={"team": "Chennai Super Kings"}
 )
+
 # update documents
 updated_doc1 = Document(
     page_content="Virat Kohli, the former captain of Royal Challengers Bangalore (RCB), is renowned for his aggressive leadership and consistent batting performances. He holds the record for the most runs in IPL history, including multiple centuries in a single season. Despite RCB not winning an IPL title under his captaincy, Kohli's passion and fitness set a benchmark for the league. His ability to chase targets and anchor innings has made him one of the most dependable players in T20 cricket.",
@@ -59,9 +68,12 @@ updated_doc1 = Document(
 )
 
 vector_store.update_document(document_id='09a39dc6-3ba6-4ea7-927e-fdda591da5e4', document=updated_doc1)
+
 # view documents
 vector_store.get(include=['embeddings','documents', 'metadatas'])
+
 # delete document
 vector_store.delete(ids=['09a39dc6-3ba6-4ea7-927e-fdda591da5e4'])
+
 # view documents
 vector_store.get(include=['embeddings','documents', 'metadatas'])
