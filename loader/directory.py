e58b4d3b7130e21b00efb1dab18f8367b874aa8f
Priyansh Singh <priyanshsingh855@gmail.com>
2025-12-25 14:25:43 +0530

    loader

---
+++ loader/directory.py
@@ -0,0 +1,15 @@
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
+
+loader = DirectoryLoader(
+    path='books',
+    glob='*.pdf',
+    loader_cls=PyPDFLoader
+)
+
+docs = loader.lazy_load()
+
+for document in docs:
+    print(document.metadata)
+
+
+# for a lot of documents use LAZY LOAD
